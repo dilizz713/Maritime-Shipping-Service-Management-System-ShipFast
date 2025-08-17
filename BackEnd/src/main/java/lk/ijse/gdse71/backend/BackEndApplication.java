4a4e2ec02a13b230b509f649ad7f91f29c8a5700
dilini <dilinichamikasilva@gmail.com>
2025-08-17 22:18:22 +0530

    add initial backend setup with Spring Boot and Maven configuration

---
+++ BackEnd/src/main/java/lk/ijse/gdse71/backend/BackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse71.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndApplication.class, args);
+    }
+
+}
