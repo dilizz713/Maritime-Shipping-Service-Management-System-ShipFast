201c3e6e48b672d508ce3a947699156b0ad861a6
dilini <dilinichamikasilva@gmail.com>
2025-08-18 11:31:10 +0530

    created customer entity and customerType (enum)

---
+++ BackEnd/src/main/java/lk/ijse/gdse71/backend/entity/Customer.java
@@ -0,0 +1,25 @@
+package lk.ijse.gdse71.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String mobileNumber;
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private CustomerType type;
+}
---
+++ BackEnd/src/main/java/lk/ijse/gdse71/backend/entity/CustomerType.java
@@ -0,0 +1,5 @@
+package lk.ijse.gdse71.backend.entity;
+
+public enum CustomerType {
+    DIRECT,THIRD_PARTY,
+}
