95555ca052d3d1078d49b3744310b48e3895000b
dilini <dilinichamikasilva@gmail.com>
2025-08-20 11:34:51 +0530

    created service interface and service impl for service entity

---
+++ BackEnd/src/main/java/lk/ijse/gdse71/backend/service/impl/ServicesServiceImpl.java
@@ -0,0 +1,11 @@
+package lk.ijse.gdse71.backend.service.impl;
+
+import lk.ijse.gdse71.backend.service.ServicesService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ServicesServiceImpl implements ServicesService {
+
+}
