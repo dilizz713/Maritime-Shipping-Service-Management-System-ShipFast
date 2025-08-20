f665053aa9fa7f82d810e5fc503d4eec4be365cc
dilini <dilinichamikasilva@gmail.com>
2025-08-20 11:35:34 +0530

    created service repository

---
+++ BackEnd/src/main/java/lk/ijse/gdse71/backend/repo/ServiceRepository.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse71.backend.repo;
+
+import lk.ijse.gdse71.backend.entity.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServiceRepository extends JpaRepository<Service, Long> {
+}
