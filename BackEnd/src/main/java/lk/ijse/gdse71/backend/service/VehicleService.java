117714cc325b98b75c394f84f1558b35928029e4
dilini <dilinichamikasilva@gmail.com>
2025-09-21 00:18:39 +0530

    Update navigation links to point to the new admin request services page and enhance provision management options

--- BackEnd/src/main/java/lk/ijse/gdse71/backend/service/VehicleService.java
+++ BackEnd/src/main/java/lk/ijse/gdse71/backend/service/VehicleService.java
@@ -1,15 +1,9 @@
 package lk.ijse.gdse71.backend.service;
 
-import aj.org.objectweb.asm.commons.Remapper;
 import lk.ijse.gdse71.backend.dto.VehicleDTO;
-import lk.ijse.gdse71.backend.dto.VehicleLocationDTO;
-import lk.ijse.gdse71.backend.entity.Vehicle;
-import lk.ijse.gdse71.backend.entity.VehicleLocation;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.time.Instant;
 import java.util.List;
-import java.util.Optional;
 
 public interface VehicleService {
 
--- BackEnd/src/main/java/lk/ijse/gdse71/backend/service/impl/VehicleServiceImpl.java
+++ BackEnd/src/main/java/lk/ijse/gdse71/backend/service/impl/VehicleServiceImpl.java
@@ -1,17 +1,12 @@
 package lk.ijse.gdse71.backend.service.impl;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
-import lk.ijse.gdse71.backend.dto.CustomerDTO;
 import lk.ijse.gdse71.backend.dto.VehicleDTO;
-import lk.ijse.gdse71.backend.entity.Customer;
 import lk.ijse.gdse71.backend.entity.Vehicle;
-import lk.ijse.gdse71.backend.exception.ResourceNotFoundException;
-import lk.ijse.gdse71.backend.repo.VehicleLocationRepository;
 import lk.ijse.gdse71.backend.repo.VehicleRepository;
 import lk.ijse.gdse71.backend.service.VehicleService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
