022a756b2f22e890b413a13e8418c455dc096f57
dilini <dilinichamikasilva@gmail.com>
2025-11-10 10:25:47 +0530

    Add Cloudinary configuration properties to application.properties and update CloudinaryConfig class

--- BackEnd/src/main/java/lk/ijse/gdse71/backend/config/CloudinaryConfig.java
+++ BackEnd/src/main/java/lk/ijse/gdse71/backend/config/CloudinaryConfig.java
@@ -2,17 +2,27 @@ package lk.ijse.gdse71.backend.config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
+    @Value("${cloudinary.cloud.name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api.key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api.secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "dbqxg7ey0",
-                "api_key", "654195966286516",
-                "api_secret", "HFUwr6MNRgZIRG-QQHL1_Hu8-R8",
+                "cloud_name",cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret,
                 "secure", true
         ));
     }
